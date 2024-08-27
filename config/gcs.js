2998d4a6bc1261c3999f574f33073cb9cb3de2a9
Ratheshan03 <lionratheshan@gmail.com>
2024-08-28 00:41:47 +0530

    chore: Update .gitignore to include firebase configuration file

--- config/gcs.js
+++ config/gcs.js
@@ -1,11 +1,4 @@
-const admin = require("firebase-admin");
-
-const serviceAccount = require("../path/to/your/serviceAccountKey.json"); // Update this path to your Firebase service account key file
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: "matrimony-site-7fa15.appspot.com", // Your Firebase Storage bucket name
-});
+const admin = require("./firebaseConfig");
 
 const bucket = admin.storage().bucket();
 
